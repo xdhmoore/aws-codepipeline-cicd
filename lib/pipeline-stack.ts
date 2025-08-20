8c241bdb22089f7ace2e13f4a52b1873240cd3f4
Daniel Moore <xdhmoore@gmail.com>
2025-08-19 20:16:16 -0700

    feat(dockerpullcache): remove priming docker pull

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -282,8 +282,8 @@ FROM gradle:6.9.1-jdk8-hotspot
         `aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${ecrUri}`,
         // Prime the ecr repo for the base image
         // `docker pull ${ecrUri}/dockerhub/library/${dockerBaseImageCli} || true`,
-        `echo "FROM ${ecrUri}/dockerhub/library/${dockerBaseImageCli}" | docker build --pull -t temp-image - || true`,
-        `docker rmi temp-image || true`,
+        // `echo "FROM ${ecrUri}/dockerhub/library/${dockerBaseImageCli}" | docker build --pull -t temp-image - || true`,
+        // `docker rmi temp-image || true`,
         `./gradlew dockerBuildImageCli -PdockerMirrorPrefix=${ecrUri}/dockerhub/library/` + " -PdockerBaseImage=" + ecrRepo.repositoryUri + `/apereo/uportal -PbaseImage=${dockerBaseImageCli}`,
         // './gradlew dockerBuildImageCli',
         // TODO use version numbers?
